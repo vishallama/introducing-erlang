f14c3486d5d3ca2a41bfbbfb3bc877491acc8720
Vishal Lama <vishal.lama@gmail.com>
2016-08-17 07:33:58 -0600

    Document fall_velocity function

--- chap02_functions_and_modules/ex5-docs/drop.erl
+++ chap02_functions_and_modules/ex5-docs/drop.erl
@@ -6,5 +6,9 @@
 -module(drop).
 -export([fall_velocity/1]).
 
+%% @doc Calculates the velocity of an object falling toward Earth, assuming
+%% no wind resistance. The distance is the height from which the object falls,
+%% specified in meters. The function returns a speed in meters per second.
+
 fall_velocity(Distance) -> math:sqrt(2 * 9.8 * Distance).
 
