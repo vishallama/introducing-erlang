c683a707f0f434d646a86c7318fc35541afcc0a2
Vishal Lama <vishal.lama@gmail.com>
2016-08-18 11:13:51 -0600

    Count up using simple recursion

---
+++ chap04_logic_and_recursion/ex9-countup/count.erl
@@ -0,0 +1,20 @@
+-module(count).
+-export([countdown/1, countup/1]).
+
+countdown(From) when From > 0 ->
+  io:format("~w!~n", [From]),
+  countdown(From - 1);
+
+countdown(_) ->
+  io:format("blastoff!~n").
+
+countup(Limit) ->
+  countup(1, Limit).
+
+countup(Count, Limit) when Count =< Limit ->
+  io:format("~w!~n", [Count]),
+  countup(Count + 1, Limit);
+
+countup(_, _) ->
+  io:format("Finished ~n").
+
