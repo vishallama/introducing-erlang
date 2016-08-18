cc91dda4663263305deb290d1cf6ce48ed11977a
Vishal Lama <vishal.lama@gmail.com>
2016-08-18 08:26:52 -0600

    Define fall_velocity/2 using case to reduce duplicate code

---
+++ chap04_logic_and_recursion/ex2-case/drop.erl
@@ -0,0 +1,11 @@
+-module(drop).
+-export([fall_velocity/2]).
+
+fall_velocity(Planemo, Distance) when Distance >= 0 ->
+  Gravity = case Planemo of
+              earth -> 9.8;
+              moon -> 1.6;
+              mars -> 3.71
+            end,
+  math:sqrt(2 * Gravity * Distance).
+
