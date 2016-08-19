f177be2c63cce03315b93a2ae137fe81ae40d2a8
Vishal Lama <vishal.lama@gmail.com>
2016-08-19 07:50:11 -0600

    Add chars function to get chars

---
+++ chap05_communicating_with_humans/ex4-ask/ask.erl
@@ -0,0 +1,10 @@
+-module(ask).
+-export([chars/0]).
+
+chars() ->
+  io:format("Which planemo are you on?~n"),
+  io:format(" 1. Earth ~n"),
+  io:format(" 2. Earth's Moon ~n"),
+  io:format(" 3. Mars ~n"),
+  io:get_chars("Which? >", 1).
+
