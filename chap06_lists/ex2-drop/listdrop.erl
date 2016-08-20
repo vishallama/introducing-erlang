9dee87d1c403cfb6361db209830e450f6554707c
Vishal Lama <vishal.lama@gmail.com>
2016-08-20 06:38:28 -0600

    Compute fall velocity for a list of planets

---
+++ chap06_lists/ex2-drop/listdrop.erl
@@ -0,0 +1,10 @@
+-module(listdrop).
+-export([falls/1]).
+
+falls(List) -> falls(List, []).
+
+falls([], Results) ->
+  lists:reverse(Results);
+falls([Head|Tail], Results) ->
+  falls(Tail, [drop:fall_velocity(Head) | Results]).
+
